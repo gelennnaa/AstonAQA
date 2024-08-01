f70cfd3fa266983ddef62058b94d920ff5dcc48b
Persona <yunavangogh@gmail.com>
2024-08-01 22:17:43 +0300

    commit

---
+++ Maven/src/main/java/com/example/Factorial.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Factorial {
+    public static long calculate(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Число не должно быть отрицательным.");
+        }
+        long result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
