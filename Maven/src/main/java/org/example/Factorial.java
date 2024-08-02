5ee12158a5328e8ad0fc4d77d9a1ca3c444db6b1
Persona <yunavangogh@gmail.com>
2024-08-02 20:57:32 +0300

    commit

--- Maven/src/main/java/org/example/Factorial.java
+++ Maven/src/main/java/org/example/Factorial.java
@@ -1,14 +1,16 @@
 package org.example;
 
+import java.math.BigInteger;
+import java.util.stream.IntStream;
+
 public class Factorial {
-    public static long calculate(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException("Число должно быть неотрицательным.");
+
+    public BigInteger getFactorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Число должно быть неотрицательным");
         }
-        long result = 1;
-        for (int i = 1; i <= number; i++) {
-            result *= i;
-        }
-        return result;
+        return IntStream.rangeClosed(1, n)
+                .mapToObj(BigInteger::valueOf)
+                .reduce(BigInteger.ONE, BigInteger::multiply);
     }
 }
---
+++ Maven/src/main/test/org/example/FactorialTest.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.testng.annotations.*;
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.testng.Assert.*;
+
+public class FactorialTest {
+    private Factorial factorial;
+
+    @BeforeClass
+    public void setUpBeforeClass() {
+        System.out.println("Начать тест");
+    }
+
+    @AfterClass
+    public void tearDownAfterClass() {
+        System.out.println("Закончить тест");
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        factorial = new Factorial();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        factorial = null;
+    }
+
+    @DataProvider(name = "factorialData")
+    public Object[][] createFactorialData() {
+        return new Object[][] {
+                {0, BigInteger.ONE},
+                {1, BigInteger.ONE},
+                {5, BigInteger.valueOf(120)},
+                {20, new BigInteger("2432902008176640000")}
+        };
+    }
+
+    @Test(dataProvider = "factorialData", priority = 1)
+    public void testFactorial(int input, BigInteger expected) {
+        System.out.printf("Запускаю testFactorial%nФакториал %d = %d%n%n", input, factorial.getFactorial(input));
+        assertEquals(factorial.getFactorial(input), expected, "Факториал " + input);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, priority = 2)
+    public void testFactorialOfNegativeNumber() {
+        System.out.println("Запускаю testFactorialOfNegativeNumber");
+        factorial.getFactorial(-1);
+    }
+}
